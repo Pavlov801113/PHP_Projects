5a1dce74e3037e0c4395050aa2b3d6c6e502e453
Pavlov801113 <139652872+Pavlov801113@users.noreply.github.com>
2023-11-18 17:19:48 +0200

    Add files via upload

---
+++ Employee.php
@@ -0,0 +1,27 @@
+<?php
+class Employee
+{
+    public $id;
+    public $name;
+    public $workingHoursPerDay = 12;
+    public $totalLeave;
+    public $workingDays;
+    public $hourlyRate = "25";
+    public $salary;
+
+    function __construct($id, $name, $totalLeave)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->totalLeave = $totalLeave;
+    }
+
+    function getSalary($totalDays){
+        $this->workingDays = $totalDays - $this->totalLeave;
+        $salary = $this->workingDays * $this->workingDays * $this->workingHoursPerDay;
+        return $salary;
+    }
+}
+$emp = new Employee(011, "Boby", 2);
+$salary = $emp->getSalary(30);
+echo $salary;
